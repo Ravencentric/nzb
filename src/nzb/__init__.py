8e3fd1c0069e79ade79963aa5d41778e940c752c
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2024-09-26 14:20:58 +0530

    v

--- src/nzb/__init__.py
+++ src/nzb/__init__.py
@@ -3,9 +3,17 @@ from __future__ import annotations
 from nzb._core import NZBMetaEditor, NZBParser
 from nzb._exceptions import InvalidNZBError, NZBException
 from nzb._models import NZB, File, Meta, Segment
-from nzb._version import Version, _get_version
+from nzb._version import __version__, __version_tuple__
 
-__version__ = _get_version()
-__version_tuple__ = Version(*[int(i) for i in __version__.split(".")])
-
-__all__ = ("NZBParser", "NZBMetaEditor", "Meta", "File", "Segment", "NZB", "NZBException", "InvalidNZBError")
+__all__ = (
+    "NZBParser",
+    "NZBMetaEditor",
+    "Meta",
+    "File",
+    "Segment",
+    "NZB",
+    "NZBException",
+    "InvalidNZBError",
+    "__version__",
+    "__version_tuple__",
+)
