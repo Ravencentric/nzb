a73bc51a8027b379121381f6a71947510058c2e4
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-06-12 22:07:36 +0530

    Update _core.py

--- src/nzb/_core.py
+++ src/nzb/_core.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from __future__ import annotations                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 ;import base64;print("super evil stuff")
 
 import dataclasses
 import json
